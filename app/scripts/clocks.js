03eae4b4421814b886f119cb9aa494f4a50999ad
James King <james@crystalpresentations.com>
2014-07-04 17:28:20 +0100

    clocks start

--- app/scripts/clocks.js
+++ app/scripts/clocks.js
@@ -1,173 +1,83 @@
 'use strict';
 
-var tiles, tile;
+var clocks, clock;
 
-tiles = {
+function timeToDeg (time) {
 
-    tileArray: [],
+    return 360 * (time / 60);
 
-    grid: {
-        rows: 10,
-        cols: 10
-    },
+}
 
-    tile: {
-        width: 40,
-        height: 40
-    },
+clocks = {
+
+    clocksArray: [],
 
     init: function () {
+        var newClock, self = this;
 
-        this.build();
+        $('.clock').each( function () {
 
-    },
+            newClock = $.extend({}, clock);
 
-    build: function () {
-        var x, y,
-            container = $('.container');
+            newClock.init($(this));
 
-        container.css({
-
-            width: this.tile.width * this.grid.cols - ( this.grid.cols - 1 ),
-
-            height: this.tile.height * this.grid.rows - ( this.grid.rows - 1 )
+            self.clocksArray.push(newClock);
 
         });
 
-        for (y = 0; y < tiles.grid.rows; y++) {
+        setInterval(function () {
 
-            for (x = 0; x < tiles.grid.cols; x++) {
+            window.requestAnimationFrame(function () {
 
-                var object = $.extend({}, tile);
+                self.update();
 
-                object.init(container);
+            });
 
-            }
+        }, 100);
+
+
+    },
+
+    update: function () {
+        var i, time = new Date();
+
+        // console.log(d.getSeconds());
+
+        for (i = 0; i < this.clocksArray.length; i++) {
+
+            this.clocksArray[i].update(time);
 
         }
+
 
     }
 
 };
 
-tile = {
+clock = {
 
     domObject: '',
 
-    front: '',
+    init: function (domObject) {
 
-    back: '',
-
-    currentTarget: '',
-
-    currentX: 0,
-
-    currentY: 0,
-
-    flipped: false,
-
-    flipping: false,
-
-    init: function (container) {
-
-        this.domObject = $('<div>', { class: 'tile'}).css({height: tiles.tile.height + 'px', width: tiles.tile.width + 'px'});
-
-        this.domObject
-            .append( $('<div>', { class: 'top target', 'data-dir': 'top' }) )
-            .append( $('<div>', { class: 'right target', 'data-dir': 'right' }) )
-            .append( $('<div>', { class: 'bottom target', 'data-dir': 'bottom' }) )
-            .append( $('<div>', { class: 'left target', 'data-dir': 'left' }) )
-            .append( $('<div>', { class: 'front' }).css({height: tiles.tile.height + 'px', width: tiles.tile.width + 'px'}) )
-            .append( $('<div>', { class: 'back' }).css({height: tiles.tile.height + 'px', width: tiles.tile.width + 'px'}) );
-
-        container.append(this.domObject);
-
-        this.front = this.domObject.children('.front');
-        this.back = this.domObject.children('.back');
-
-        // this.domObject.css('background', 'tomato');
-
-        this.listeners();
-         console.log(this.currentY + ', ' + -this.currentY);
+        this.domObject = domObject;
 
     },
 
-    listeners: function () {
-        var self = this;
+    update: function (time) {
+        var s, m, h;
 
-        this.domObject
+        // console.log(time);
 
-            .on('mouseenter', function (event) {
+        s = time.getSeconds();
+        m = time.getMinutes();
+        h = time.getHours();
 
-                // console.log ('enter ' + $(event.toElement).data('dir'));
+        console.log(this.domObject);
 
-                // self.doEnter($(event.toElement).data('dir'));
+        this.domObject.children('.s-hand').css('transform','rotate(' + timeToDeg(s) + 'deg)');
+        this.domObject.children('.m-hand').css('transform','rotate(' + timeToDeg(m) + 'deg)');
 
-                self.domObject.css('background', 'red');
-
-                self.flipped = true;
-
-            })
-
-            .on('mouseleave', function () {
-
-                // console.log ('exit ' + self.currentTarget);
-
-                if (self.flipped) {
-
-                    setTimeout(function(){
-
-                        // self.doEnter(self.currentTarget);
-                        self.domObject.css('background', '#222');
-
-                    },1000);
-
-                    self.flipped = false;
-
-                }
-
-            })
-
-            .children('.target').on('mouseenter', function (event) {
-
-                self.currentTarget = $(event.target).data('dir');
-
-            });
-
-    },
-
-    doEnter: function (direction) {
-
-        // var direction = $(event.toElement).data('dir');
-        var difX = 0, difY = 0;
-
-        // var theNum = cheese ? 180 : -180;
-        var theNum = 180;
-
-        switch (direction) {
-            case 'top':
-                difX = theNum;
-            break;
-
-            case 'bottom':
-                difX = -theNum;
-            break;
-
-            case 'right':
-                difY = -theNum;
-            break;
-
-            case 'left':
-                difY = theNum;
-            break;
-        }
-
-        this.currentX += difX;
-        this.currentY += difY;
-        // console.log(this.currentY);
-
-        this.front.css('transform', 'rotateX(' + this.currentX + 'deg) rotateY(' + (this.currentY * -1) + 'deg)');
-        this.back.css('transform', 'rotateX(' + (this.currentX + 180) + 'deg) rotateY(' + this.currentY + 'deg)');
 
     }
 
@@ -175,6 +85,10 @@ tile = {
 
 $(function() {
 
-    tiles.init();
+    if ( $('body').hasClass('clocks') ) {
+
+        clocks.init();
+
+    }
 
 });
